e33890ceeb1024ebf3800a03c670a6f9ae4d0a34
jrary <73732743+jrary@users.noreply.github.com>
2021-05-19 06:56:46 +0900

    Update Guindata.sql

--- Guindata.sql
+++ Guindata.sql
@@ -1,1 +1,10 @@
 SELECT * FROM guindata.user;
+CREATE TABLE `guindata`.`user` (
+  `id` INT NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `address` VARCHAR(45) NOT NULL,
+  `phone` INT(11) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `occupation` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
