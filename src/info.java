b7b15692a530f7e31d80acf7be8d03787fb89707
kimgihean <83570546+kimgihean@users.noreply.github.com>
2021-05-19 14:30:56 +0900

    Create info.java

---
+++ src/info.java
@@ -0,0 +1,10 @@
+import java.util.scanner;
+
+public class info {
+  private String name;
+  private String id;
+  private String password;
+  private String email;
+  private String address;
+  
+}
